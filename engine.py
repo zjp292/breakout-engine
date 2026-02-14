d2fc7829d79fea596a3f3ebe064df9d2c284d9f4
zjp292 <zacharyparham0@gmail.com>
2026-02-14 16:54:48 -0700

    load pickle

--- engine.py
+++ engine.py
@@ -1,11 +1,15 @@
+import pickle
+
+
 class Engine:
     def __init__(
         self,
     ):
         pass
 
-    def load_pickle(self,):
-        
+    def load_pickle(self, file):
+        with open(file, "rb") as f:
+            return pickle.load(file)
 
     def create_features(self):
         pass
